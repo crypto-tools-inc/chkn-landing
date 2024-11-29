8f052cdf14c36e8a4b4d9520af506b45e412c610
monzanifabio <renderbros@gmail.com>
2024-11-29 13:41:40 +0000

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,46 @@
+// Array of image URLs (replace with your actual image paths)
+const messageImages = ["img/slide-1.png", "img/slide-2.png", "img/slide-3.png"];
+
+const messagesContainer = document.getElementById("messagesContainer");
+let currentImageIndex = 0;
+
+function createMessageElement(imageUrl) {
+  const messageDiv = document.createElement("div");
+  messageDiv.classList.add("message");
+
+  const img = document.createElement("img");
+  img.src = imageUrl;
+  img.alt = "Chat Message";
+  img.classList.add("img-fluid");
+
+  messageDiv.appendChild(img);
+  return messageDiv;
+}
+
+function addNewMessage() {
+  // Create and add new message
+  const newMessage = createMessageElement(messageImages[currentImageIndex]);
+  messagesContainer.appendChild(newMessage);
+
+  // Trigger reflow to enable transition
+  void newMessage.offsetWidth;
+
+  // Add visible class to animate with delay
+  requestAnimationFrame(() => {
+    requestAnimationFrame(() => {
+      newMessage.classList.add("visible");
+
+      // Scroll to bottom
+      // Scroll the container to the bottom to show the newly added message
+      messagesContainer.scrollTop = messagesContainer.scrollHeight - messagesContainer.clientHeight;
+    });
+  });
+
+  // Update index for next iteration
+  currentImageIndex = (currentImageIndex + 1) % messageImages.length;
+}
+// Initial setup
+addNewMessage();
+
+// Add messages every 3 seconds
+setInterval(addNewMessage, 2000);
